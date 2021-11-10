a08494cda3f957e7dd1a1a987ad2a2deeb1bbf07
yattew <yathansh@gmail.com>
2021-11-10 19:55:22 +0530

    hemlo guys and gals

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    1
+    <?php
+        print "<div>hemlo friends</div>";
+    ?>
+    2
+</body>
+</html>
