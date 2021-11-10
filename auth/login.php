89f4a784217e125d82ccca5a613f3e703e569a3c
osbins <arpitjain.2001@gmail.com>
2021-11-10 23:58:04 +0530

    initialised login page, (set up PHP yada yada)

---
+++ auth/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | MiniKiosk</title>
+</head>
+
+<body>
+    <?php echo "This is a test PHP page."
+    ?>
+</body>
+
+</html>
