16bf208b5b386f74f6333306a72079ab2961062a
osbins <arpitjain.2001@gmail.com>
2021-11-11 00:51:24 +0530

    my bad pt.2

--- auth/index.php
+++ auth/index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/index.css">
+    <link rel="stylesheet" href="static/index.css">
     <title>Login | MiniKiosk</title>
 </head>
 
