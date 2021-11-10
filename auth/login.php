de5cca9661971e622d14b133e0f3135eae448086
osbins <arpitjain.2001@gmail.com>
2021-11-11 00:17:20 +0530

    created basic login page structure

--- auth/login.php
+++ auth/login.php
@@ -3,12 +3,20 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/login.css">
     <title>Login | MiniKiosk</title>
 </head>
 
 <body>
-    <?php echo "This is a test PHP page."
-    ?>
+    <div id="container">
+        <h3>MiniKiosk</h3>
+        <div id="form-wrapper">
+            <form action="#">
+                Enrolment Number: <input type="number" name="login" id="enrol">
+                Password: <input type="password" name="login" id="pass">
+            </form>
+        </div>
+    </div>
 </body>
 
 </html>
