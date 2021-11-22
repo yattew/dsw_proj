9d43d5691fe4fdc0408099fca3b8f2d1eb582398
osbins <arpitjain.2001@gmail.com>
2021-11-22 15:40:09 +0530

    did some stuff

--- student/index.php
+++ student/index.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 include "../partials/session.php";
 include "../partials/messages.php";
 if(has_messages())
@@ -6,4 +6,24 @@ if(has_messages())
     show_messages();
     delete_messages();
 }
-?>
+?> -->
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="static/student.css">
+    <title>Student Dashboard | MiniKiosk</title>
+    <link rel="shortcut icon" href="assets/favicon.png" type="image/x-icon">
+</head>
+
+<body>
+<div id="navbar">
+    <h3>MiniKiosk</h3>
+    <div id="nav-items">
+        <div class="nav-item"> <a href="studentProfile.php">Profile</a></div>
+        <div class="nav-item"><a href="#">Timetable</a></div>
+        <div class="nav-item"><a href="#">Attendance</a></div>
+        <div class="nav-item"><a href="#">Result</a></div>
+        <div class="nav-item"><a href="#">Logout</a></div>
+    </div>
+</div>
+</body>
