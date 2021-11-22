8c0955721642ca05d1bca9ddfaff10ceb23e30eb
yattew <yathansh@gmail.com>
2021-11-22 15:44:36 +0530

    added the navbar partial

---
+++ partials/navbar.php
@@ -0,0 +1,15 @@
+<?php
+$navbar = '
+<div id="navbar">
+    <h3>MiniKiosk</h3>
+    <div id="nav-items">
+        <div class="nav-item"> <a href="studentProfile.php">Profile</a></div>
+        <div class="nav-item"><a href="#">Timetable</a></div>
+        <div class="nav-item"><a href="#">Attendance</a></div>
+        <div class="nav-item"><a href="#">Result</a></div>
+        <div class="nav-item"><a href="#">Logout</a></div>
+    </div>
+</div>
+';
+echo $navbar;
+?>
--- student/index.php
+++ student/index.php
@@ -16,14 +16,5 @@ if(has_messages())
 </head>
 
 <body>
-<div id="navbar">
-    <h3>MiniKiosk</h3>
-    <div id="nav-items">
-        <div class="nav-item"> <a href="studentProfile.php">Profile</a></div>
-        <div class="nav-item"><a href="#">Timetable</a></div>
-        <div class="nav-item"><a href="#">Attendance</a></div>
-        <div class="nav-item"><a href="#">Result</a></div>
-        <div class="nav-item"><a href="#">Logout</a></div>
-    </div>
-</div>
+<?php include "../partials/navbar.php"  ?>
 </body>
