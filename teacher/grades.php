10dbae320faf3316b0f8d3c90c1e753023aa6744
osbins <arpitjain.2001@gmail.com>
2021-11-27 17:12:07 +0530

    updated grades CSS

--- teacher/grades.php
+++ teacher/grades.php
@@ -7,15 +7,12 @@ include "../partials/navbarTeacher.php"
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "<br><br>";
-    foreach($_POST as $key=>$val)
-    {
-        echo $key ." : " . $val ."<br>";
+    foreach ($_POST as $key => $val) {
+        echo $key . " : " . $val . "<br>";
     }
     unset($_SESSION["batch"]);
-}
-else{
-    if(!isset($_SESSION["batch"]))
-    {
+} else {
+    if (!isset($_SESSION["batch"])) {
         header("location: select_batch.php");
     }
 }
@@ -35,37 +32,40 @@ else{
     $students = array("yathansh", "arpit", "nishttha", "simran");
     $batch = $_SESSION["batch"];
     unset($_SESSION["batch"]);
-    ?>
-    <form action="attendance.php" method="POST">
-        batch: <?php echo $batch ?>
-        <br>
-        <table>
-            <tr>
-                <th>name</th>
-                <th>A</th>
-                <th>B</th>
-                <th>C</th>
-                <th>D</th>
-                <th>E</th>
-                <th>F</th>
-            </tr>
-            <?php
-            foreach ($students as $student) {
-                echo "<tr>";
-                echo "<td> $student </td>";
-                echo "<td><input type='radio' name='$student' value='A'></td>";
-                echo "<td><input type='radio' name='$student' value='B'></td>";
-                echo "<td><input type='radio' name='$student' value='C'></td>";
-                echo "<td><input type='radio' name='$student' value='D'></td>";
-                echo "<td><input type='radio' name='$student' value='E'></td>";
-                echo "<td><input type='radio' name='$student' value='F'></td>";
-                echo "</tr>";
-            }
-            ?>
-        </table>
-            <input type="submit" value="save">
-    </form>
-    <a href="select_batch.php">select_batch</a>
+    ?> <div id="grades-wrapper">
+        <form action="attendance.php" method="POST">
+            <h4> Batch: <?php echo $batch ?> </h4>
+            <br>
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>A</th>
+                    <th>B</th>
+                    <th>C</th>
+                    <th>D</th>
+                    <th>E</th>
+                    <th>F</th>
+                </tr>
+                <?php
+                foreach ($students as $student) {
+                    echo "<tr>";
+                    echo "<td> $student </td>";
+                    echo "<td><input type='radio' name='$student' value='A'></td>";
+                    echo "<td><input type='radio' name='$student' value='B'></td>";
+                    echo "<td><input type='radio' name='$student' value='C'></td>";
+                    echo "<td><input type='radio' name='$student' value='D'></td>";
+                    echo "<td><input type='radio' name='$student' value='E'></td>";
+                    echo "<td><input type='radio' name='$student' value='F'></td>";
+                    echo "</tr>";
+                }
+                ?>
+            </table>
+            <div class="btn"><input type="submit" value="Save">
+            </div>
+        </form>
+        <small style="color: red; margin-top: 10px;">Grades have been updated</small>
+        <a href="select_batch.php">Select a different batch</a>
+    </div>
 </body>
 
 </html>
--- teacher/select_batch.php
+++ teacher/select_batch.php
@@ -5,8 +5,7 @@ include "../partials/faculty_login_required.php";
 include "../partials/navbarTeacher.php"
 ?>
 <?php
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $batch = $_POST["batch"];
     $_SESSION["batch"] = $batch;
     header("location: grades.php");
@@ -14,28 +13,33 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="static/teacher.css">
-    <title>Faculty Dashboard | MiniKiosk</title>
+    <title>Grades | MiniKiosk</title>
     <link rel="shortcut icon" href="assets/favicon.png" type="image/x-icon">
 </head>
+
 <body>
     <?php
-        $batches = array("B2","B1","B3");
+    $batches = array("B2", "B1", "B3");
     ?>
-    <form action="select_batch.php" method="POST">
-        select batch
-        <select name="batch" id="">
-            <?php
-            foreach($batches as $batch)
-            {
-                echo "<option value='$batch'>$batch</option>";
-            }
-            ?>
-            
-        </select>
-        <input type="submit" value="select">
-    </form>
+
+    <div id="form-wrapper">
+        <form action="select_batch.php" method="POST">
+            <h2>Select Batch</h2><br> <div id="batch">
+            <select name="batch" id="">
+                <?php
+                foreach ($batches as $batch) {
+                    echo "<option value='$batch'>$batch</option>";
+                }
+                ?>
+
+            </select> </div><br> <div class="btn">
+            <input type="submit" value="Select"> </div>
+        </form>
+    </div>
 </body>
+
 </html>
