6f1811a8e27e0447313a335d168b1d8ed9de805b
yattew <yathansh@gmail.com>
2021-12-10 00:19:42 +0530

    update_teacher now working

--- admin/list_teachers.php
+++ admin/list_teachers.php
@@ -6,12 +6,14 @@ include "../partials/sql_connect.php";
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <table>
         <tr>
@@ -27,19 +29,35 @@ include "../partials/sql_connect.php";
             <th>
                 DEPT
             </th>
+            <th>
+                EDIT
+            </th>
         </tr>
         <?php
-            $sql = "select id,name,phno,dept from faculty";
-            $teachers = mysqli_query($conn,$sql);
-            foreach ($teachers as $teacher) {
-                echo '<tr>';
-                foreach($teacher as $key => $val)
-                {
-                    echo "<td>$val</td>";
-                }
-                echo '</tr>';
+        $sql = "select id,name,phno,dept from faculty";
+        $teachers = mysqli_query($conn, $sql);
+        foreach ($teachers as $teacher) {
+            echo '<tr>';
+            foreach ($teacher as $key => $val) {
+                echo "<td>$val</td>";
             }
+            $id = $teacher["id"];
+            echo "<td><form action='update_teacher.php'>
+                <input type='number' name='id' id='id' hidden value='$id'>
+                <input type='submit' value='edit'>
+                </form></td>";
+            echo '</tr>';
+        }
         ?>
     </table>
+    <div id="credStat"><?php
+                        if (has_messages()) {
+                            echo "<div id='errors'>";
+                            show_messages();
+                            delete_messages();
+                            echo "</div>";
+                        }
+                        ?></div>
 </body>
+
 </html>
