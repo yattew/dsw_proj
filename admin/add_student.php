c7027003930ad19f7cef7dd30c1cc4400c153992
osbins <arpitjain.2001@gmail.com>
2021-12-09 21:51:10 +0530

    css-ed add_student

--- admin/add_student.php
+++ admin/add_student.php
@@ -4,25 +4,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Add Student | MiniKiosk</title>
+    <link rel="stylesheet" href="static/admin.css">
 </head>
-<body>
-    <form action="add_student.php">
-        id:<input type="number" name="id" id="id">
+<body style='background-image: url("assets/jaypee.jpg"); background-size: 100%; background-attachment: fixed;'>
+<div id="navbar"> 
+    <div id="title"><h3>MiniKiosk</h3><small>Admin Panel</small></div>
+    <div id="nav-items">
+    <div class="nav-item"><a href="/dsw_proj/auth/logout.php">Logout</a></div>
+    </div>
+</div>
+<div id="form-wrapper">
+    <h3>ADD STUDENT</h3> <br><br>
+    <form action="add_student.php" method="POST">
+        ID <br><input type="number" name="id" id="id">
         <br>
-        name:<input type="text" name="name" id="name">
+        Name <br><input type="text" name="name" id="name">
         <br>
-        dob: <input type="date" name="dob" id="dob">
+        DOB <br><input type="date" name="dob" id="dob">
         <br>
-        semester: <input type="number" name="semester" id="semester">
+        Semester <br><input type="number" name="semester" id="semester">
         <br>
-        batch: <input type="text" name="batch" id="batch">
+        Batch <br><input type="text" name="batch" id="batch">
         <br>
-        cource: <input type="text" name="course" id="course">
+        Course <br><input type="text" name="course" id="course">
         <br>
-        password: <input type="text" name="password" id="password">
+        Set Password <br><input type="text" name="password" id="password">
         <br>
-        <input type="submit" value="save">
+        <input type="submit" value="Save" onclick="checkBlankFields();">
     </form>
+</div>
+<script src="static/admin.js"></script>
 </body>
 </html>
