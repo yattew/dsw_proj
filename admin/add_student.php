be2f3eb1a9850559ad26adbef05d557141752d3e
yattew <yathansh@gmail.com>
2021-12-09 20:19:43 +0530

    added stuff

--- admin/add_student.php
+++ admin/add_student.php
@@ -7,6 +7,22 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="add_student.php">
+        id:<input type="number" name="id" id="id">
+        <br>
+        name:<input type="text" name="name" id="name">
+        <br>
+        dob: <input type="date" name="dob" id="dob">
+        <br>
+        semester: <input type="number" name="semester" id="semester">
+        <br>
+        batch: <input type="text" name="batch" id="batch">
+        <br>
+        cource: <input type="text" name="course" id="course">
+        <br>
+        password: <input type="text" name="password" id="password">
+        <br>
+        <input type="submit" value="save">
+    </form>
 </body>
 </html>
