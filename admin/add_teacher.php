6f5b3bd32db094cf7154ec93c1ba4006e581da7a
yattew <yathansh@gmail.com>
2021-12-09 20:25:35 +0530

    added stuff

--- admin/add_teacher.php
+++ admin/add_teacher.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <form action="add_teacher.php">
+        id:<input type="number" name="id" id="id">
+        <br>
+        name:<input type="text" name="name" id="name">
+        <br>
+        phno: <input type="number" name="phno" id="phno">
+        <br>
+        password: <input type="text" name="password" id="password">
+        <br>
+        dept: <input type="text" name="dept" id="dept">
+        <br>
+        <input type="submit" value="save">
+    </form>
 </body>
+
 </html>
