2b2737216611314f56d197cd214fe2e04cc35378
yattew <yathansh@gmail.com>
2021-12-09 22:24:11 +0530

    add_student working now

--- admin/add_student.php
+++ admin/add_student.php
@@ -1,5 +1,48 @@
+<?php
+include "../partials/session.php";
+include "../partials/messages.php";
+include "../partials/sql_connect.php";
+?>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = $_POST["id"];
+    $name = $_POST["name"];
+    $dob = $_POST["dob"];
+    $semester = $_POST["semester"];
+    $batch = $_POST["batch"];
+    $course = $_POST["course"];
+    $password = $_POST["password"];
+    $sql = "
+        select * from student where id = $id; 
+    ";
+    if (mysqli_num_rows(mysqli_query($conn, $sql)) > 0) {
+        set_message("student already exists");
+    } else {
+        $sql = "
+            insert into student values(
+                $id,
+                '$name',
+                '$dob',
+                '$semester',
+                '$batch',
+                '$course',
+                '$password'
+                )
+        ";
+        $res = mysqli_query($conn, $sql);
+        if ($res) {
+            set_message("student successfully added");
+        } else {
+            set_message("error in adding student");
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,33 +50,45 @@
     <title>Add Student | MiniKiosk</title>
     <link rel="stylesheet" href="static/admin.css">
 </head>
+
 <body style='background-image: url("assets/jaypee.jpg"); background-size: 100%; background-attachment: fixed;'>
-<div id="navbar"> 
-    <div id="title"><h3>MiniKiosk</h3><small>Admin Panel</small></div>
-    <div id="nav-items">
-    <div class="nav-item"><a href="/dsw_proj/auth/logout.php">Logout</a></div>
+    <div id="navbar">
+        <div id="title">
+            <h3>MiniKiosk</h3><small>Admin Panel</small>
+        </div>
+        <div id="nav-items">
+            <div class="nav-item"><a href="/dsw_proj/auth/logout.php">Logout</a></div>
+        </div>
     </div>
-</div>
-<div id="form-wrapper">
-    <h3>ADD STUDENT</h3> <br><br>
-    <form action="add_student.php" method="POST">
-        ID <br><input type="number" name="id" id="id">
-        <br>
-        Name <br><input type="text" name="name" id="name">
-        <br>
-        DOB <br><input type="date" name="dob" id="dob">
-        <br>
-        Semester <br><input type="number" name="semester" id="semester">
-        <br>
-        Batch <br><input type="text" name="batch" id="batch">
-        <br>
-        Course <br><input type="text" name="course" id="course">
-        <br>
-        Set Password <br><input type="text" name="password" id="password">
-        <br>
-        <input type="submit" value="Save" onclick="checkBlankFields();">
-    </form>
-</div>
-<script src="static/admin.js"></script>
+    <div id="form-wrapper">
+        <h3>ADD STUDENT</h3> <br><br>
+        <form action="add_student.php" method="POST">
+            ID <br><input type="number" name="id" id="id">
+            <br>
+            Name <br><input type="text" name="name" id="name">
+            <br>
+            DOB <br><input type="date" name="dob" id="dob">
+            <br>
+            Semester <br><input type="number" name="semester" id="semester">
+            <br>
+            Batch <br><input type="text" name="batch" id="batch">
+            <br>
+            Course <br><input type="text" name="course" id="course">
+            <br>
+            Set Password <br><input type="text" name="password" id="password">
+            <br>
+            <input type="submit" value="Save" onclick="checkBlankFields();">
+        </form>
+        <div id="credStat"><?php
+                            if (has_messages()) {
+                                echo "<div id='errors'>";
+                                show_messages();
+                                delete_messages();
+                                echo "</div>";
+                            }
+                            ?></div>
+    </div>
+    <script src="static/admin.js"></script>
 </body>
+
 </html>
